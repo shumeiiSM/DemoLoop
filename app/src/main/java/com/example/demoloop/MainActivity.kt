4028e516f541c7f22a863fbf49b47f0b9e0ca28b
17010233 <17010233@myrp.edu.sg>
2019-08-08 13:08:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoloop/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.demoloop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+
+            // FOR METHOD
+            for (i in 1..5) {
+                Log.d("MainActivity", "$i")
+            }
+
+            // WHILE METHOD
+//            var i = 1
+//            while (i < 6) {
+//                Log.d("MainActivity", "$i")
+//                i++
+//            }
+
+        }
+
+        button2.setOnClickListener {
+            val word = editText.text.toString()
+
+            for (letter in word) {
+                Log.d("MainActivity", "$letter")
+            }
+        }
+
+    }
+}
